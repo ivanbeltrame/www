8b679319ae2f1ee990c33808f7beb19246787228
Ivan Beltrame <87547424+ivanbeltrame@users.noreply.github.com>
2025-04-06 16:04:42 +0200

    Rebuild website with a new look!

--- js/app.js
+++
@@ -1,7 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js")
-        .then((reg) => console.log("Service worker registered", reg))
-        .catch((err) => console.log("Service worker not registered", err));
-} else {
-    console.log("Service worker not supported");
-}
